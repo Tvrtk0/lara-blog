8ac9170f72d3a8edb269592673b36cf954660173
Tvrtko <tvrtkobabic2@gmail.com>
2024-05-12 00:14:51 +0200

    configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://Tvrtk0.github.io',
+  base: 'lara-blog',
 });
